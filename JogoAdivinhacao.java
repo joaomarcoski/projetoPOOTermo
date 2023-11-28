5d0cd10f1048914eea8280639373a5e7857bcdaf
joaomarcoski <joaomarcoski22@gmail.com>
2023-11-28 16:52:36 -0300

    Termo v.2

--- JogoAdivinhacao.java
+++ JogoAdivinhacao.java
@@ -1,3 +1,4 @@
+package projetoPOOTermo;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+package projetoPOOTermo;
 import java.util.Arrays;
 import java.util.List;
 
